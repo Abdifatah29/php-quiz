7c9a29da86b39510bfdb99dbeb86b440dde76e3e
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-08-21 01:03:11 +0200

    added html form tags

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Link for stylesheet -->
+  <link href="css/style.css" rel="stylesheet">
+  <!--  -->
+  <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+  <!-- Title tag -->
+  <title>Quiz questionnaire</title>
+  <style>
+   h2{
+       font-size: 32px red;
+   }
+  </style>
 </head>
 <body>
-    <?php
-    echo "whatsup";
-    ?>
+    <!-- form to record user input -->
+    <form action="quiz.php" method="post">
+    </form>
+    <!-- End form to record user input -->
 </body>
-</html>
