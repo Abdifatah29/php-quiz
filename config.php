dec1cea3ce511045cde90825f8fbe509dc96d94a
Abdifatah29 <abdi.hassannoor@onedayonly.co.za>
2021-05-20 23:56:14 +0200

    moved data to config

---
+++ config.php
@@ -0,0 +1,229 @@
+<?php
+
+$questionnaire = [
+     [
+        'question' => ' What is the largest type of ‘big cat’ in the world?',
+        'quessTheAnswer' => [
+            'A' => 'Lion',
+            'B' => 'Tiger',
+            'C' => 'Black Panther',
+            'D' => 'Cheeteh'
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+    [
+        'question' => ' What is the fastest land animal in the world?',
+        'quessTheAnswer' => [
+            'A' => 'Cheetah',
+            'B' => 'Ostrich',
+            'C' => 'Giraffe',
+            'D' => 'Chicken'
+        ],
+        'correctAnswer' => 'A'
+    ],
+
+    [
+        'question' => ' What are female elephants called?',
+        'quessTheAnswer' => [
+            'A' => 'Lioness',
+            'B' => 'Cow',
+            'C' => 'A female elephant',
+            'D' => 'Eagle'
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+    [
+        'question' => ' Groups of lions are known as what?',
+        'quessTheAnswer' => [
+            'A' => 'Empire',
+            'B' => 'Royalty',
+            'C' => 'Prides',
+            'D' => 'LIon kingdom'
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+    [
+        'question' => ' What type of animal is the largest primate in the world?',
+        'quessTheAnswer' => [
+            'A' => 'Elephant',
+            'B' => 'Whale',
+            'C' => 'Gorilla',
+            'D' => 'Mouse'
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+    [
+        'question' => ' How many legs does a spider have?',
+        'quessTheAnswer' => [
+            'A' => 12,
+            'B' => 6,
+            'C' => 21,
+            'D' => 8
+        ],
+        'correctAnswer' => 'D'
+    ],
+
+        [
+        'question' => ' What is the most recognizable physical feature of the male lion?',
+        'quessTheAnswer' => [
+            'A' => 'Its claws',
+            'B' => 'Weight',
+            'C' => 'Its mane',
+            'D' => 'Its pride'
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+        [
+        'question' => ' How many pairs of wings does a bee have?',
+        'quessTheAnswer' => [
+            'A' => 4,
+            'B' => 2,
+            'C' => 6,
+            'D' => 10
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+        [
+        'question' => ' What are baby goats called?',
+        'quessTheAnswer' => [
+            'A' => 'Cristiano Ronaldo',
+            'B' => 'Kids',
+            'C' => 'Baby goat',
+            'D' => 'Snakes'
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+        [
+        'question' => ' Are butterflies insects?',
+        'quessTheAnswer' => [
+            'A' => 'No',
+            'B' => 'Yes',
+            'C' => "'I don't Know",
+            'D' => 'Maybe'
+        ],
+        'correctAnswer' => 'B'
+    ],
+    //End of wildlife questions
+
+    //History related questionnaires
+
+         [
+        'question' => ' What was the Nazi symbol called?',
+        'quessTheAnswer' => [
+            'A' => 'Swastika',
+            'B' => 'Captain John Alcock',
+            'C' => "German power",
+            'D' => "Hitler's child"
+        ],
+        'correctAnswer' => 'A'
+    ],
+
+    [
+        'question' => ' In which year was the death of Queen Elizabeth I?',
+        'quessTheAnswer' => [
+            'A' => 1903,
+            'B' => 1605,
+            'C' => 1994,
+            'D' => 1603
+        ],
+        'correctAnswer' => 'D'
+    ],
+
+    [
+        'question' => ' Name the second largest city in Britain during the Black Death?',
+        'quessTheAnswer' => [
+            'A' => 'Wincester',
+            'B' => 'Bristol',
+            'C' => 'Cardiff',
+            'D' => 'London'
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+    [
+        'question' => ' Which war first introduced the use of tanks?',
+        'quessTheAnswer' => [
+            'A' => 1800,
+            'B' => 1937,
+            'C' => 1961,
+            'D' => 1918
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+    [
+        'question' => ' Which country did Britain fight in the War of Jenkins’s Ear?',
+        'quessTheAnswer' => [
+            'A' => 'Spain',
+            'B' => 'Portugal',
+            'C' => 'France',
+            'D' => 'Italy'
+        ],
+        'correctAnswer' => 'A'
+    ],
+
+    [
+        'question' => ' What year did Adolf Hitler die?',
+        'quessTheAnswer' => [
+            'A' => 1977,
+            'B' => 2003,
+            'C' => 1950,
+            'D' => 1945
+        ],
+        'correctAnswer' => 'D'
+    ],
+    [
+        'question' => ' In which decade did compulsory driving testing begin? ',
+        'quessTheAnswer' => [
+            'A' => 1935,
+            'B' => 1934,
+            'C' => 1903,
+            'D' => 1989
+        ],
+        'correctAnswer' => 'A'
+    ],
+
+    [
+        'question' => ' In which year was the Wall Street Crash?',
+        'quessTheAnswer' => [
+            'A' => 1918,
+            'B' => 1987,
+            'C' => 1929,
+            'D' => 1907
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+    [
+        'question' => ' During which year did the RMS Titanic sink?',
+        'quessTheAnswer' => [
+            'A' => 1997,
+            'B' => 1918,
+            'C' => 1912,
+            'D' => 1890
+        ],
+        'correctAnswer' => 'C'
+    ],
+
+    [
+        'question' => ' Who led the Million Man March on Washington?',
+        'quessTheAnswer' => [
+            'A' => 'Michael Jackson',
+            'B' => 'Louis Farrakhan',
+            'C' => "Donald Trump",
+            'D' => 'Louis Hernandez'
+        ],
+        'correctAnswer' => 'B'
+    ],
+
+
+    // <!-- //Multiple answers for Wildlife questions -->
+    // <!-- //End of history related questionnaires -->
+];
